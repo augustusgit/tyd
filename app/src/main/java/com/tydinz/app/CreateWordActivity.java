4dd00f7150ad4d23cc262cf5cc1416fcc846ba82
Augustus Irechukwu <augustusnwangwu@gmail.com>
2021-06-22 17:08:02 +0100

    Add word UI Completed

--- app/src/main/java/com/tydinz/app/CreateWordActivity.java
+++ app/src/main/java/com/tydinz/app/CreateWordActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -14,14 +16,24 @@ public class CreateWordActivity extends AppCompatActivity {
 
 
     private ArrayList<String> mImageUrls = new ArrayList<>();
+    final EditText aWordTxt = findViewById(R.id.aWordbxt);
+    final EditText bWordTxt = findViewById(R.id.bWordbxt);
+    final EditText cWordTxt = findViewById(R.id.cWordbxt);
+    final EditText dWordTxt = findViewById(R.id.dWordbxt);
+    final EditText eWordTxt = findViewById(R.id.eWordbxt);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_word);
 
-        getImages();
+//        getImages();
+
     }
+
+
+
+
 
     private void getImages(){
         mImageUrls.add("https://www.googleapis.com/drive/v3/files/1L6GUHCfUIt6o1Boe2sUOKcGTI3Qgqejs?alt=media&key=AIzaSyAk_9xL_HyMFzKxMPfHIli0EQHhXuLmNls&v=.png");
@@ -45,19 +57,39 @@ public class CreateWordActivity extends AppCompatActivity {
 
         mImageUrls.add("https://www.googleapis.com/drive/v3/files/1p-HYq9hkpRoWOw4pbRHHDN2rd5YfJ1UC?alt=media&key=AIzaSyAk_9xL_HyMFzKxMPfHIli0EQHhXuLmNls&v=.png");
 
-        initRecyclerView();
+//        initRecyclerView();
     }
 
-    private void initRecyclerView(){
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(layoutManager);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mImageUrls, this);
-        recyclerView.setAdapter(adapter);
-    }
+//    private void initRecyclerView(){
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+//        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+//        recyclerView.setLayoutManager(layoutManager);
+//        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mImageUrls, this);
+//        recyclerView.setAdapter(adapter);
+//    }
 
 
     public void onClick(View v) {
         super.onBackPressed(); // or super.finish();
+    }
+
+    public void aWordClick(View view) {
+        aWordTxt.setVisibility(View.VISIBLE);
+    }
+
+    public void bWordClick(View view) {
+        bWordTxt.setVisibility(View.VISIBLE);
+    }
+
+    public void cWordClick(View view) {
+        cWordTxt.setVisibility(View.VISIBLE);
+    }
+
+    public void dWordClick(View view) {
+        dWordTxt.setVisibility(View.VISIBLE);
+    }
+
+    public void eWordClick(View view) {
+        eWordTxt.setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/com/tydinz/app/EWordActivity.java
@@ -0,0 +1,14 @@
+package com.tydinz.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_e_word);
+    }
+}
